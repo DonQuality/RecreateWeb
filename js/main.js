c3e0e7803af640b96c3e2113da59190f738cec4a
DonQuality <santosemmanuel@outlook.com>
2015-03-04 09:43:27 -0800

    updates JS

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
-$('#showDropMenu').on('click', function(){
-  $('#dropMenu').slideToggle();
+// shows hidden menu
+$('.showDropMenu').on('click', function(){
+  $('.dropMenu').slideToggle(150);
 });
-
-$('#showSearchBar').on('click', function(){
+// shows input bar
+$('.showSearchBar').on('click', function(){
   $('input[type="text"]').slideToggle(100);
 });
-
+// shows alert div box
 $('[role="alert"]').on('click', function(){
-  $('<p>Connect to start receiving private recommendations \n from your friends</p>').appendTo('#el-signIn').delay(4000).fadeOut(500);
-  $('[role="alert"]').hide().delay(4050).fadeIn(250);
+  $('<p>Connect to start receiving private recommendations \n from your friends</p>').appendTo('.el-signIn').delay(4000).fadeOut(500);
+  $('[role="alert"]').hide().delay(4000).fadeIn(250);
 });
+
+$('.show-menu-drop').on('click', function(){
+  $('.menu-drop-down').slideToggle(150);
+})
+
+
+
 
 
